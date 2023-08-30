bf9f6fe5b16a35811d85118b1874475796702ab3
LukaPodrug <lukapodra1@gmail.com>
2023-08-30 18:54:54 +0200

    Game table cell layout

---
+++ GameApp - UIKit/Controllers/GameTableViewCell.swift
@@ -0,0 +1,12 @@
+//
+//  GameTableViewCell.swift
+//  GameApp - UIKit
+//
+//  Created by Luka Podrug on 30.08.2023..
+//
+
+import UIKit
+
+class GameTableViewCell: UITableViewCell {
+    @IBOutlet var gameLabel: UILabel!
+}
