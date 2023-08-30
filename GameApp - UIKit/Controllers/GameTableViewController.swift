54122d63ff80b8799543721de4a690d7eff339c2
LukaPodrug <lukapodra1@gmail.com>
2023-08-30 15:07:31 +0200

    Game table view navigation bar

--- GameApp - UIKit/Controllers/GameTableViewController.swift
+++ GameApp - UIKit/Controllers/GameTableViewController.swift
@@ -10,9 +10,17 @@ import UIKit
 class GameTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
+        
+        title = "Games"
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Settings", style: .done, target: self, action: #selector(showGenreSelectionModal))
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 0
+    }
+    
+    @objc func showGenreSelectionModal() {
+        
     }
 }
