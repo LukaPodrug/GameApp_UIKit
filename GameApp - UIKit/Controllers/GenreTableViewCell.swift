9a60eb4ea261ff03868c7e019e4ec01e20a00e73
LukaPodrug <lukapodra1@gmail.com>
2023-08-30 16:41:32 +0200

    Genre table cell layout

---
+++ GameApp - UIKit/Controllers/GenreTableViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  GenreTableViewCell.swift
+//  GameApp - UIKit
+//
+//  Created by Luka Podrug on 30.08.2023..
+//
+
+import UIKit
+
+class GenreTableViewCell: UITableViewCell {
+    @IBOutlet var genreLabel: UILabel!
+    @IBOutlet var genreSwitch: UISwitch!
+}
--- GameApp - UIKit/Controllers/GenreTableViewController.swift
+++ GameApp - UIKit/Controllers/GenreTableViewController.swift
@@ -12,16 +12,24 @@ class GenreTableViewController: UITableViewController {
         super.viewDidLoad()
         
         title = "Genres"
+        
+        tableView.register(UINib(nibName: "GenreTableCellView", bundle: nil), forCellReuseIdentifier: "GenreCell")
+        
         guard let _ = UserDefaults().string(forKey: "selectedGenres") else {
             navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Confirm", style: .plain, target: self, action: #selector(submitGenres))
             navigationItem.rightBarButtonItem?.isEnabled = false
             return
         }
-        
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        return 0
+        return 5
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "GenreCell", for: indexPath) as! GenreTableViewCell
+        
+        return cell
     }
     
     @objc func submitGenres() {
