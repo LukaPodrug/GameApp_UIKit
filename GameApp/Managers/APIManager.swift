8dc89c29205ff4b6ba613b9e8359cfcbe13c76f9
LukaPodrug <lukapodra1@gmail.com>
2024-02-02 09:46:33 +0100

    Managers pattern

---
+++ GameApp/Managers/APIManager.swift
@@ -0,0 +1,8 @@
+//
+//  APIManager.swift
+//  GameApp
+//
+//  Created by Luka Podrug on 02.02.2024..
+//
+
+import Foundation
---
+++ GameApp/Managers/UserDefaultsManager.swift
@@ -0,0 +1,8 @@
+//
+//  UserDefaultsManager.swift
+//  GameApp
+//
+//  Created by Luka Podrug on 02.02.2024..
+//
+
+import Foundation
