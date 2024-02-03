83c4439d7258bd5699db4cad8cc8d41b5407bf83
LukaPodrug <lukapodra1@gmail.com>
2024-02-03 15:24:56 +0100

    Get all genres

--- GameApp/Managers/APIManager.swift
+++ GameApp/Managers/APIManager.swift
@@ -8,9 +8,91 @@
 import Foundation
 import Combine
 
+enum APIError: Error {
+    case invalidURL
+    case missingResponse
+    case user
+    case server
+    case unknown
+}
+
+extension APIError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+            case .invalidURL:
+                return NSLocalizedString("Invalid URL", comment: "")
+            case .missingResponse:
+                return NSLocalizedString("Missing response", comment: "")
+            case .user:
+                return NSLocalizedString("User error", comment: "")
+            case .server:
+                return NSLocalizedString("Server error", comment: "")
+            case .unknown:
+                return NSLocalizedString("Unknown error", comment: "")
+        }
+    }
+}
+
 class APIManager {
     static let shared: APIManager = APIManager()
     
+    var cancellables = Set<AnyCancellable>()
+    
+    let baseAPIURL: String = "https://api.rawg.io/api"
+    
+    func getAllGenres() -> Future<GenresResponseModel, Error> {
+        let fullAPIURL: String = baseAPIURL + "/genres" + "?key=\(APIKey)"
+        
+        return Future<GenresResponseModel, Error> { promise in
+            guard let url = URL(string: fullAPIURL) else {
+                return promise(.failure(APIError.invalidURL))
+            }
+            
+            var request = URLRequest(url: url)
+            request.httpMethod = "GET"
+            
+            URLSession.shared.dataTaskPublisher(for: request)
+                .receive(on: DispatchQueue.main)
+                .tryMap { (data, response) -> Data in
+                    guard let httpResponse = response as? HTTPURLResponse else {
+                        throw APIError.missingResponse
+                    }
+
+                    if 400...499 ~= httpResponse.statusCode {
+                        throw APIError.user
+                    }
+                    
+                    else if 500...599 ~= httpResponse.statusCode {
+                        throw APIError.server
+                    }
+                    
+                    else if !(200...299 ~= httpResponse.statusCode) {
+                        throw APIError.unknown
+                    }
+                    
+                    return data
+                }
+                .decode(type: GenresResponseModel.self, decoder: JSONDecoder())
+                .sink(receiveCompletion: { completion in
+                    if case let .failure(error) = completion {
+                        switch error {
+                            case let decodingError as DecodingError:
+                                promise(.failure(decodingError))
+                            case let apiError as APIError:
+                                promise(.failure(apiError))
+                            default:
+                                promise(.failure(APIError.unknown))
+                        }
+                    }
+                }, receiveValue: { data in
+                    promise(.success(data))
+                })
+                .store(in: &self.cancellables)
+        }
+    }
+}
+
+extension APIManager {
     private var APIKey: String {
         get {
             guard let configPlistFilePath: String = Bundle.main.path(forResource: "Config", ofType: "plist") else {
@@ -25,9 +107,5 @@ class APIManager {
             
             return APIKey
         }
-    }
-    
-    func getAllGameGenres() {
-
     }
 }
