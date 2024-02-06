4546a741f8a9bd2ea2e965f137ac9bcbbf2527ca
LukaPodrug <lukapodra1@gmail.com>
2024-02-06 16:28:17 +0100

    Add firebase

--- GameApp/Startup/AppDelegate.swift
+++ GameApp/Startup/AppDelegate.swift
@@ -5,12 +5,16 @@
 //  Created by Luka Podrug on 01.02.2024..
 //
 
+import FirebaseCore
 import UIKit
 
 @main
 class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
+        
+        FirebaseApp.configure()
+        
         return true
     }
 
